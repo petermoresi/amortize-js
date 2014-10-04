558557ae0d8ae4a5fc1b7f9d12fac2d74d382a6c
Peter Moresi <peter.moresi@gmail.com>
2014-10-03 22:18:46 -0700

    Minor improvements to demo page

--- amortize.js
+++ amortize.js
@@ -7,6 +7,7 @@ function pmt(rate,nper,pv) {
 
     return pmt;
 };
+
 function compute_schedule(loan_amount, interest_rate, payments_per_year, years, payment) {
     var schedule = [];
     var remaining = loan_amount;
@@ -15,7 +16,7 @@ function compute_schedule(loan_amount, interest_rate, payments_per_year, years, 
     for (var i=0; i<=number_of_payments; i++) {
         var interest = remaining * (interest_rate/100/payments_per_year);
         var principle = (payment-interest);
-        var row = [i, payment.toFixed(2), principle.toFixed(2), interest.toFixed(2), remaining.toFixed(2)];
+        var row = [i, principle.toFixed(2), interest.toFixed(2), remaining.toFixed(2)];
         schedule.push(row);
         remaining -= principle
     }
